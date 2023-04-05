b852a9460f62db16336c13cc63a538a09818ad32
Lachlan Grose <lachlan.grose@monash.edu>
2023-04-05 15:44:19 +1000

    fix adding test for widget

---
+++ test/test_widget.py
@@ -0,0 +1,39 @@
+import unittest
+
+import geo_trace
+from geo_trace.geo_trace_dockwidget import GeoTraceDockWidget
+from qgis.core import QgsProject, QgsRasterLayer
+from qgis.testing.mocked import get_iface
+
+iface = get_iface()
+
+
+class TestTraceInput(unittest.TestCase):
+    def load_widget(self):
+        self.widget = GeoTraceDockWidget(iface)
+        self.widget.show()
+    def add_raster_layer(self,name):
+        rlayer = QgsRasterLayer('/storage/test/data/DEM.tif', name)
+        QgsProject.instance().addMapLayer(rlayer)
+
+    def check_layer_exists(self, name):
+        names = [layer.name() for layer in QgsProject.instance().mapLayers().values()]
+        assert name in names
+
+    def test_load_widget(self):
+        self.load_widget()
+    
+    def test_cost_calculator_button(self):
+        self.load_widget()
+        self.add_raster_layer('test')
+        self.widget.button_click(name="cc_brightness")
+        self.check_layer_exists('test_brightness')
+    
+
+    
+        
+        
+
+
+if __name__ == "__main__":
+    unittest.main()
